564ab86129ca8bdc40ae02e3aacce5951d2e8570
mardonmashreq <mardon.mashreq@gmail.com>
2019-05-27 14:10:12 +0400

    removed enzyme, added react-testing-library

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: "ts-jest",
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  testRegex: "(/__tests__/.*|(\\.|/)(test))\\.(jsx?|tsx?)$",
   collectCoverage: true,
-  setupFilesAfterEnv: ["<rootDir>/src/setupEnzyme.ts"]
 };
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,16 +2,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as enzyme from 'enzyme';
+import { render } from "react-testing-library";
 
 describe('<App/>', () => {
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
+  
   it('has a "Hello, world!"', () => {
-    const wrapper = enzyme.shallow(<App />);
-    expect(wrapper.contains('Hello, world!'))
+    const { getByText } = render(<App/>);
+    getByText('Hello, world!')
   });
+  
 });
